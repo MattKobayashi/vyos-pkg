9c250485d9fccd41fa8e8173950da06d24fec161
Matthew Kobayashi <matthew@kobayashi.au>
2024-05-20 19:55:08 +1000

    feat: Build packages for all release trains at once

--- .github/scripts/build_repo.sh
+++ .github/scripts/build_repo.sh
@@ -10,26 +10,26 @@ generate_hashes() {
   done
 }
 
-main() {
-  DEB_POOL="_site/${SUITE}/deb/pool/${COMPONENTS:-main}"
-  DEB_DISTS="dists/${SUITE:-stable}"
+repo_equuleus() {
+  DEB_POOL="_site/equuleus/deb/pool/${COMPONENTS:-main}"
+  DEB_DISTS="dists/equuleus"
   DEB_DISTS_COMPONENTS="${DEB_DISTS}/${COMPONENTS:-main}/binary-all"
   GPG_TTY=""
   export GPG_TTY
-  pushd _site/${SUITE}/deb >/dev/null
+  pushd _site/equuleus/deb >/dev/null
   mkdir -p "${DEB_DISTS_COMPONENTS}"
   echo "Scanning all downloaded DEB Packages and creating Packages file."
   dpkg-scanpackages --arch all pool/ > "${DEB_DISTS_COMPONENTS}/Packages"
   gzip -9 > "${DEB_DISTS_COMPONENTS}/Packages.gz" < "${DEB_DISTS_COMPONENTS}/Packages"
   bzip2 -9 > "${DEB_DISTS_COMPONENTS}/Packages.bz2" < "${DEB_DISTS_COMPONENTS}/Packages"
   popd >/dev/null
-  pushd "_site/${SUITE}/deb/${DEB_DISTS}" >/dev/null
+  pushd "_site/equuleus/deb/${DEB_DISTS}" >/dev/null
   echo "Making Release file"
   {
     echo "Origin: ${ORIGIN}"
     echo "Label: ${REPO_OWNER}"
-    echo "Suite: ${SUITE:-stable}"
-    echo "Codename: ${SUITE:-stable}"
+    echo "Suite: equuleus"
+    echo "Codename: equuleus"
     echo "Version: 1.0"
     echo "Architectures: all"
     echo "Components: ${COMPONENTS:-main}"
@@ -45,4 +45,79 @@ main() {
   echo "DEB repo built"
   popd >/dev/null
 }
-main
+
+repo_sagitta() {
+  DEB_POOL="_site/sagitta/deb/pool/${COMPONENTS:-main}"
+  DEB_DISTS="dists/sagitta"
+  DEB_DISTS_COMPONENTS="${DEB_DISTS}/${COMPONENTS:-main}/binary-all"
+  GPG_TTY=""
+  export GPG_TTY
+  pushd _site/sagitta/deb >/dev/null
+  mkdir -p "${DEB_DISTS_COMPONENTS}"
+  echo "Scanning all downloaded DEB Packages and creating Packages file."
+  dpkg-scanpackages --arch all pool/ > "${DEB_DISTS_COMPONENTS}/Packages"
+  gzip -9 > "${DEB_DISTS_COMPONENTS}/Packages.gz" < "${DEB_DISTS_COMPONENTS}/Packages"
+  bzip2 -9 > "${DEB_DISTS_COMPONENTS}/Packages.bz2" < "${DEB_DISTS_COMPONENTS}/Packages"
+  popd >/dev/null
+  pushd "_site/sagitta/deb/${DEB_DISTS}" >/dev/null
+  echo "Making Release file"
+  {
+    echo "Origin: ${ORIGIN}"
+    echo "Label: ${REPO_OWNER}"
+    echo "Suite: sagitta"
+    echo "Codename: sagitta"
+    echo "Version: 1.0"
+    echo "Architectures: all"
+    echo "Components: ${COMPONENTS:-main}"
+    echo "Description: ${DESCRIPTION:-A repository for packages released by ${REPO_OWNER}}"
+    echo "Date: $(date -Ru)"
+    generate_hashes MD5Sum md5sum
+    generate_hashes SHA1 sha1sum
+    generate_hashes SHA256 sha256sum
+  } > Release
+  echo "Signing Release file"
+  gpg --detach-sign --armor --sign > Release.gpg < Release
+  gpg --detach-sign --armor --sign --clearsign > InRelease < Release
+  echo "DEB repo built"
+  popd >/dev/null
+}
+
+repo_circinus() {
+  DEB_POOL="_site/circinus/deb/pool/${COMPONENTS:-main}"
+  DEB_DISTS="dists/circinus"
+  DEB_DISTS_COMPONENTS="${DEB_DISTS}/${COMPONENTS:-main}/binary-all"
+  GPG_TTY=""
+  export GPG_TTY
+  pushd _site/circinus/deb >/dev/null
+  mkdir -p "${DEB_DISTS_COMPONENTS}"
+  echo "Scanning all downloaded DEB Packages and creating Packages file."
+  dpkg-scanpackages --arch all pool/ > "${DEB_DISTS_COMPONENTS}/Packages"
+  gzip -9 > "${DEB_DISTS_COMPONENTS}/Packages.gz" < "${DEB_DISTS_COMPONENTS}/Packages"
+  bzip2 -9 > "${DEB_DISTS_COMPONENTS}/Packages.bz2" < "${DEB_DISTS_COMPONENTS}/Packages"
+  popd >/dev/null
+  pushd "_site/circinus/deb/${DEB_DISTS}" >/dev/null
+  echo "Making Release file"
+  {
+    echo "Origin: ${ORIGIN}"
+    echo "Label: ${REPO_OWNER}"
+    echo "Suite: circinus"x
+    echo "Codename: circinus"
+    echo "Version: 1.0"
+    echo "Architectures: all"
+    echo "Components: ${COMPONENTS:-main}"
+    echo "Description: ${DESCRIPTION:-A repository for packages released by ${REPO_OWNER}}"
+    echo "Date: $(date -Ru)"
+    generate_hashes MD5Sum md5sum
+    generate_hashes SHA1 sha1sum
+    generate_hashes SHA256 sha256sum
+  } > Release
+  echo "Signing Release file"
+  gpg --detach-sign --armor --sign > Release.gpg < Release
+  gpg --detach-sign --armor --sign --clearsign > InRelease < Release
+  echo "DEB repo built"
+  popd >/dev/null
+}
+
+repo_equuleus
+repo_sagitta
+repo_circinus
---
+++ index.md
@@ -0,0 +1,40 @@
+---
+layout: home
+title: Unofficial VyOS .deb Packages Repository
+---
+Since the maintainers of the VyOS project have [decided](https://blog.vyos.io/community-contributors-userbase-and-lts-builds) that they won't permit external access to their own package repositories anymore, I've set up my own unofficial one.
+
+## How to use
+
+### Using with `vyos-build` for `equuleus` release train
+
+Run the `configure` script from the [`vyos-build` repository](https://github.com/vyos/vyos-build/tree/equuleus), passing the --vyos-mirror argument. Then run `sudo make iso`:
+
+```
+sudo ./configure --architecture amd64 --build-by "j.randomhacker@vyos.io" --vyos-mirror "[trusted=yes] {{ site.url }}/vyos-pkg/equuleus/deb"
+sudo make iso
+```
+
+### Using with `vyos-build` for `sagitta` release train
+
+Run the `build-vyos-image` script from the [`vyos-build` repository](https://github.com/vyos/vyos-build/tree/sagitta), passing the --vyos-mirror argument:
+
+```
+sudo ./build-vyos-image iso --architecture amd64 --build-by "j.randomhacker@vyos.io" --vyos-mirror "[trusted=yes] {{ site.url }}/vyos-pkg/sagitta/deb"
+```
+
+### Using with `vyos-build` for `circinus` (or `current`) release train
+
+Run the `build-vyos-image` script from the [`vyos-build` repository](https://github.com/vyos/vyos-build/tree/current), passing the --vyos-mirror argument:
+
+```
+sudo ./build-vyos-image iso --architecture amd64 --build-by "j.randomhacker@vyos.io" --vyos-mirror "[trusted=yes] {{ site.url }}/vyos-pkg/circinus/deb"
+```
+
+## How to contribute?
+
+Please contribute changes and bug reports in the relevant repository above.
+
+Have a security issue? Please email [Matthew](mailto:matthew@kobayashi.au) with details.
+
+Have your own action? Please create a [pull request on this repository](https://mattkobayashi.github.io/vyos-pkg.github.io/pulls).
