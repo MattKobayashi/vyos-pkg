644368523e01a4c0416393dd2ee2ceeace876103
Matthew Kobayashi <matthew@kobayashi.au>
2024-05-29 20:33:15 +1000

    Grab .debs from Jenkins

--- .github/scripts/get_debs.py
+++ .github/scripts/get_debs.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python3
 
 import os
-import urllib.request
-import json
+import jenkins
+import requests
 
-with urllib.request.urlopen("https://api.github.com/repos/MattKobayashi/vyos-pkg/releases") as response:
-	response_json = response.read()
-	response_dict = json.loads(response_json)
+# if os.environ.get("JENKINS_SERVER").endswith("/"):
+# 	jenkins_url = os.environ.get("JENKINS_SERVER")[:-1]
+# else:
+# 	jenkins_url = os.environ.get("JENKINS_SERVER")
+jenkins_url = os.environ.get("JENKINS_SERVER")
 
-os.makedirs("_site/equuleus/deb/pool/main", exist_ok=True)
-break_loop = False
-for release in response_dict:
-	if "equuleus" in release["tag_name"]:
-		for asset in release["assets"]:
-			urllib.request.urlretrieve(asset["browser_download_url"], "_site/equuleus/deb/pool/main/" + asset["name"])
-		break_loop = True
-	if break_loop == True:
-		break
+server = jenkins.Jenkins(
+	url=os.environ.get("JENKINS_SERVER"),
+	username=os.environ.get("JENKINS_USERNAME"),
+	password=os.environ.get("JENKINS_API_KEY")
+	)
+jobs = server.get_jobs()
 
-os.makedirs("_site/sagitta/deb/pool/main", exist_ok=True)
-break_loop = False
-for release in response_dict:
-	if "sagitta" in release["tag_name"]:
-		for asset in release["assets"]:
-			urllib.request.urlretrieve(asset["browser_download_url"], "_site/sagitta/deb/pool/main/" + asset["name"])
-		break_loop = True
-	if break_loop == True:
-		break
+releases = ['equuleus', 'sagitta', 'current']
 
-os.makedirs("_site/current/deb/pool/main", exist_ok=True)
-break_loop = False
-for release in response_dict:
-	if "current" in release["tag_name"]:
-		for asset in release["assets"]:
-			urllib.request.urlretrieve(asset["browser_download_url"], "_site/current/deb/pool/main/" + asset["name"])
-		break_loop = True
-	if break_loop == True:
-		break
+for release_train in releases:
+	os.makedirs(f'./_site/{release_train}/deb/pool/main/')
+	for folder in jobs:
+		if folder['name'] == f'vyos-{release_train}':
+			for job in folder['jobs']:
+				job_name = f'vyos-{release_train}/' + job['name']
+				try:
+					last_successful_build = server.get_job_info(job_name, 0, True)["lastSuccessfulBuild"]["number"]
+					build_info = server.get_build_info(job_name, last_successful_build)
+					artifacts = server.get_build_info(job_name, last_successful_build)["artifacts"]
+					for artifact in artifacts:
+						artifact_data = server.get_build_artifact_as_bytes(job_name, last_successful_build, artifact['relativePath'])
+						with open(f'./_site/{release_train}/deb/pool/main/' + artifact["fileName"], 'wb') as artifact_file:
+							artifact_file.write(artifact_data)
+					# for artifact in artifacts:
+					# 	artifact_data = requests.request(
+					# 		method='GET',
+					# 		url=f'{jenkins_url}/job/vyos-{release_train}/job/{job_name}/{last_successful_build}/artifact/{artifact["relativePath"]}',
+					# 		auth=(os.environ.get("JENKINS_USERNAME"), os.environ.get("JENKINS_API_KEY")),
+					# 		stream=True
+					# 		)
+					# 	with open(f'./_site/{release_train}/deb/pool/main/' + artifact["fileName"], 'wb') as artifact_file:
+					# 		for chunk in artifact_data.raw(chunk_size=1024):
+					# 			artifact_file.write(chunk)
+				except TypeError:
+					pass
