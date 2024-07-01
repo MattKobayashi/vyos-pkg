1b18751bd042736d2efea1665165b6d3d49831e0
Matthew Kobayashi <matthew@kobayashi.au>
2024-07-01 14:36:44 +1000

    Move to functions

--- .github/scripts/parse_jf.py
+++ .github/scripts/parse_jf.py
@@ -5,40 +5,39 @@ from datetime import datetime
 import json_repair
 import subprocess
 import os
+import tomllib
 
-# Read Jenkinsfile
-with open('Jenkinsfile') as myfile:
-    content = myfile.read()
 
-# Initialise the pkglist_raw string
-pkglist_raw = re.search(r'def pkgList = \[\n.*?\n\]', content, re.DOTALL) \
+def parseJenkinsfile(content: str) -> list:
+    # Initialise the pkglist_raw string
+    pkglist_raw = re.search(r'def pkgList = \[\n.*?\n\]', content, re.DOTALL) \
                 .group() \
                 .removeprefix('def pkgList = ')
+    # Other replacements
+    pkglist_raw = pkglist_raw.replace('[', '{')
+    pkglist_raw = pkglist_raw.replace(']', '}')
+    pkglist_raw = pkglist_raw.replace('// ', '# ')
+    pkglist_raw = pkglist_raw.replace("'''", "'")
+    # Replace timestamp placeholder if it exists
+    if 'def timestamp = ' in content:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        pkglist_raw = pkglist_raw.replace('${timestamp}', timestamp)
+    # Replace commit_id placeholder if it exists
+    if 'def commit_id = ' in content:
+        commit_id = re.search(r'def commit_id = \'[0-9a-fA-F]{7}\'', content, re.DOTALL) \
+                        .group() \
+                        .removeprefix('def commit_id = \'') \
+                        .removesuffix('\'')
+        pkglist_raw = pkglist_raw.replace('${commit_id}', commit_id)
+        pkglist_raw = pkglist_raw.replace('commit_id', commit_id)
+    # Convert pkglist_raw to JSON
+    pkglist = json_repair.loads(pkglist_raw)
+    return pkglist
 
-# Other replacements
-pkglist_raw = pkglist_raw.replace('[', '{')
-pkglist_raw = pkglist_raw.replace(']', '}')
-pkglist_raw = pkglist_raw.replace('// ', '# ')
-pkglist_raw = pkglist_raw.replace("'''", "'")
 
-# Replace timestamp placeholder if it exists
-if 'def timestamp = ' in content:
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    pkglist_raw = pkglist_raw.replace('${timestamp}', timestamp)
-
-# Replace commit_id placeholder if it exists
-if 'def commit_id = ' in content:
-    commit_id = re.search(r'def commit_id = \'[0-9a-fA-F]{7}\'', content, re.DOTALL) \
-                    .group() \
-                    .removeprefix('def commit_id = \'') \
-                    .removesuffix('\'')
-    pkglist_raw = pkglist_raw.replace('${commit_id}', commit_id)
-    pkglist_raw = pkglist_raw.replace('commit_id', commit_id)
-
-# Convert pkglist_raw to JSON
-pkglist = json_repair.loads(pkglist_raw)
-
-if isinstance(pkglist, list):
+def getBuildCmds(pkglist) -> list:
+    if not isinstance(pkglist, list):
+        pkglist = [pkglist]
     for package in pkglist:
         # Handle Linux kernel build
         if '\n' in package['buildCmd']:
@@ -46,52 +45,45 @@ if isinstance(pkglist, list):
             cmdlist = list(filter(None, cmdlist))
             cmd = ' &&'.join(cmdlist)
             package['buildCmd'] = cmd
-            kernel_ver = dict(
-                os.environ, \
-                KERNEL_VER=subprocess.run(['cat ../../data/defaults.toml | tomlq -r .kernel_version'], shell=True, capture_output=True) \
-                .stdout.decode('utf-8') \
-                .rstrip()
-                )
-        # Do `git clone` and `git checkout` things
-        if 'scmUrl' in package:
-            subprocess.run(['git', 'clone', package['scmUrl'], package['name']])
-            subprocess.run(['git', 'checkout', package['scmCommit']], cwd=package['name'])
         # Split build commands
         commands = re.split('&&|;(?! then)', package['buildCmd'])
-        # Run each build command
-        for index, command in enumerate(commands):
-            if 'cd ..' in command:
-                continue
-            elif 'cd ..' in commands[index-1] or 'scmUrl' not in package:
-                subprocess.run(command, shell=True, env=kernel_ver)
-                continue
-            else:
-                subprocess.run(command, shell=True, cwd=package['name'])
-else:
-    # Handle Linux kernel build
-    if '\n' in pkglist['buildCmd']:
-        cmdlist = pkglist['buildCmd'].split('\n')
-        cmdlist = list(filter(None, cmdlist))
-        cmd = ' &&'.join(cmdlist)
-        pkglist['buildCmd'] = cmd
-        kernel_ver = dict(
-            os.environ, \
-            KERNEL_VER=subprocess.run(['cat ../../data/defaults.toml | tomlq -r .kernel_version'], shell=True, capture_output=True) \
-            .stdout.decode('utf-8') \
-            .rstrip()
-            )
+        # Strip whitespace
+        commands = [command.strip() for command in commands]
+        # Remove comments
+        commands = [command for command in commands if not command.startswith('#')]
+        return commands
+
+
+# Read Jenkinsfile
+with open('Jenkinsfile') as jenkinsfile:
+    content = jenkinsfile.read()
+
+# Parse Jenkinsfile
+pkglist = parseJenkinsfile(content)
+
+# If it ain't a list, make it one
+if not isinstance(pkglist, list):
+    pkglist = [pkglist]
+
+# Main package loop
+for package in pkglist:
     # Do `git clone` and `git checkout` things
-    if 'scmUrl' in pkglist:
-        subprocess.run(['git', 'clone', pkglist['scmUrl'], pkglist['name']])
-        subprocess.run(['git', 'checkout', pkglist['scmCommit']], cwd=pkglist['name'])
-    # Split build commands
-    commands = re.split('&&|;(?! then)', pkglist['buildCmd'])
+    if 'scmUrl' in package:
+        subprocess.run(['git', 'clone', package['scmUrl'], package['name']])
+        subprocess.run(['git', 'checkout', package['scmCommit']], cwd=package['name'])
+    # Get build commands
+    commands = getBuildCmds(package)
     # Run each build command
-    for index, command in enumerate(commands):
-        if 'cd ..' in command:
-            continue
-        elif 'cd ..' in commands[index-1] or 'scmUrl' not in pkglist:
-            subprocess.run(command, shell=True, env=kernel_ver)
-            continue
-        else:
-            subprocess.run(command, shell=True, cwd=pkglist['name'])
+    if 'linux-kernel' in os.getcwd():
+        # Extract kernel version from defaults.toml
+        with open('../../data/defaults.toml', 'rb') as f:
+            defaults_toml = tomllib.load(f)
+        kernel_version = defaults_toml['kernel_version']
+        # Remove flow control
+        for index, command in enumerate(commands):
+            commands[index] = re.sub(r'\${KERNEL_VER}', kernel_version, command)
+            if command in ['if { $? -ne 0 }; then', 'exit 1', 'fi']:
+                commands.pop(index)
+        subprocess.run('; '.join(commands), shell=True)
+    else:
+        subprocess.run('; '.join(commands), shell=True, cwd=package['name'])
